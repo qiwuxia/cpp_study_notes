cb00fc838d951cfe15117c7e939081db76a99583
renxiaoyang <103484971+qiwuxia@users.noreply.github.com>
2023-10-13 11:21:50 +0800

    1

--- 练习/7/7-4person.cpp
+++ 练习/7/7-4person.cpp
@@ -8,12 +8,22 @@
 #include <cstdlib>
 
 using namespace std;
-
+istream &read(istream &in, Person_messge &ins);
 class Person_messge{
-private:
+public:
     string name;
     string address;
-public:
     string Name() const { return name; } // 这两个函数应该被定义为常量成员函数，因为不论返回姓名还是返回地址，在函数体内部都知识堆区数据成员的值，而不会做任何改变
     string Address() const { return address; }
 };
+
+istream &read(istream &in, Person_messge &ins)
+{
+    in >> ins.name >> ins.address;
+    return in;
+}
+ostream &printf_person(ostream &ou, Person_messge &out)
+{
+    ou << "姓名：" << out.Name() << " " << "地址：" << out.Address();
+    return ou;
+}
