dd5c739c19db5bef6f27c476d3a4280c31d1b5d0
renxiaoyang <2401848539@qq.com>
2023-11-29 17:17:19 +0800

    1

--- practice/8/0801.cpp
+++ practice/8/0801.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-istream& rcin(istream& cs)
+istream &rcin(istream &cs)
 {
     char s;
     string ss;
     cs >> s;
-    while(!cs.eof())
+    while (!cs.eof())
     {
         ss += s;
         if (cs.fail())
@@ -17,14 +17,36 @@ istream& rcin(istream& cs)
         }
         cs >> s;
     }
-    cout << ss << endl;
+    cout << "eof:"
+         << "  " << ss << endl;
+    cin.clear();
+    cs >> s;
+    while (!cs.fail())
+    {
+        ss += s;
+        if (cs.eof())
+        {
+            cs.clear();
+            continue;
+        }
+        cs >> s;
+        auto now = cs.fail();
+        auto now2 = cs.eof();
+        auto now3 = cs.bad()
+    }
+    cout << "fail:"
+         << "  " << ss << endl;
+
     return cs;
 }
 
-int main ()
+int main()
 {
     string str;
     int i = 5;
-    rcin(cin);
+    int i_eof, i_fail;
+    cout << unitbuf;
+    while ()
+
     return 0;
 }
